fec30f627bce9ac32ebb0e353b48dcc24be0223c
Zhengqian Kuang <dev.kuang@gmail.com>
2022-08-06 12:11:11 -0600

    React Navigation: Getting started - done

--- src/components/hello-world/style.ts
+++ src/components/hello-world/style.ts
@@ -2,14 +2,22 @@ import {StyleSheet} from 'react-native';
 import {colors, Theme} from '../../themes';
 
 export const fixedStyles = StyleSheet.create({
-  //
+  rowView: {
+    flexDirection: 'row',
+  },
 });
 
 export const themedStyles = (theme: Theme) => {
   return StyleSheet.create({
-    safeArea: {
-      flex: 1,
+    baseView: {
+      width: '100%',
+      height: '100%',
       backgroundColor: colors(theme).background,
+      justifyContent: 'center', // along primary axis
+      alignItems: 'center', // along secondary axis
+    },
+    text: {
+      color: colors(theme).text,
     },
   });
 };
--- src/components/root/App.tsx
+++ src/components/root/App.tsx
@@ -1,51 +1,21 @@
 import React from 'react';
-import {SafeAreaView, StatusBar, Text, View} from 'react-native';
-import {lStr, isChinese} from '../../assets/strings';
-import {useChangeLanguage, useTheme} from '../hooks';
-import {Button} from '../styled';
+import {SafeAreaView, StatusBar} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {HelloWorld} from '../hello-world';
+import {useTheme} from '../hooks';
 import {themedStyles} from './style';
 
 export const App = () => {
   const theme = useTheme();
-  const changeLanguage = useChangeLanguage();
 
   return (
-    <SafeAreaView style={themedStyles(theme).safeArea}>
-      <StatusBar
-        barStyle={theme === 'dark' ? 'light-content' : 'dark-content'}
-      />
-      <View style={themedStyles(theme).baseView}>
-        <View style={{flexDirection: 'row'}}>
-          <Button
-            title={'Eng-\nlish'}
-            flavor={'ios-bordered'}
-            onPress={() => {
-              changeLanguage('en');
-            }}
-            textProps={{numberOfLines: 2}}
-          />
-          <Button
-            title={'中\n文'}
-            flavor={'ios-bordered'}
-            onPress={() => {
-              changeLanguage('zh_CN');
-            }}
-            textProps={{
-              numberOfLines: 2,
-              style: {
-                fontWeight: 'bold',
-                color: 'red',
-              },
-            }}
-          />
-        </View>
-        <Text style={themedStyles(theme).text}>{lStr('helloWorld')}</Text>
-        {!isChinese() && (
-          <Text style={themedStyles(theme).text}>
-            {lStr('iHaveSomeChildren', {count: 2})}
-          </Text>
-        )}
-      </View>
-    </SafeAreaView>
+    <NavigationContainer>
+      <SafeAreaView style={themedStyles(theme).safeArea}>
+        <StatusBar
+          barStyle={theme === 'dark' ? 'light-content' : 'dark-content'}
+        />
+        <HelloWorld />
+      </SafeAreaView>
+    </NavigationContainer>
   );
 };
