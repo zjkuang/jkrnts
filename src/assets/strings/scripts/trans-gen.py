bd5c2d38c1b2dc7bd1bd5c23b3f3da6ded3bdb30
Zhengqian Kuang <dev.kuang@gmail.com>
2022-08-07 22:07:58 -0600

    py in progress

--- src/assets/strings/scripts/trans-gen.py
+++ src/assets/strings/scripts/trans-gen.py
@@ -2,11 +2,19 @@
 # In VSCode, Command-Shift-P, "Python: Select interpreter", select the one marked "Global"
 
 import yaml
+from os import listdir
+from os.path import isfile, join
 
 print('yaml version:', yaml.__version__)
 
+print('List yaml files:')
+onlyfiles = [f for f in listdir('../yaml') if isfile(join('../yaml', f))]
+for f in onlyfiles:
+  print(' - ', f)
+print('End of list.')
+
 if __name__ == '__main__':
-  with open ("../yaml/greetings.yaml", "r") as foo:
+  with open ('../yaml/greetings.yaml', 'r') as foo:
     dict = yaml.safe_load(foo)
 
   for key, value in dict.items():
