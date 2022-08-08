29d426c786998736a25b4fa0390034542fa09240
Zhengqian Kuang <dev.kuang@gmail.com>
2022-08-08 08:02:55 -0600

    py in progress

--- src/assets/strings/scripts/trans-gen.py
+++ src/assets/strings/scripts/trans-gen.py
@@ -7,15 +7,35 @@ from os.path import isfile, join
 
 print('yaml version:', yaml.__version__)
 
-print('List yaml files:')
-onlyfiles = [f for f in listdir('../yaml') if isfile(join('../yaml', f))]
-for f in onlyfiles:
-  print(' - ', f)
-print('End of list.')
+yamldir = '../yaml'
+
+def getFileList():
+  onlyfiles = [f for f in listdir(yamldir) if isfile(join(yamldir, f))]
+  return onlyfiles
+
+def testGetFileList(filelist):
+  print('List yaml files:')
+  for f in filelist:
+    print(' - ', f)
+  print('End of list.')
+
+def loadFile(file):
+  with open(join(yamldir, file), 'r') as f:
+    o = yaml.safe_load(f)
+    return o
 
 if __name__ == '__main__':
-  with open ('../yaml/greetings.yaml', 'r') as foo:
-    dict = yaml.safe_load(foo)
-
-  for key, value in dict.items():
-    print(key + ":" + str(value))
+  files = getFileList()
+  for f in files:
+    o = loadFile(f)
+    print('Contents in ', f)
+    print(type(o))
+    if isinstance(o, dict):
+      print('Is dict')
+    print(o)
+    for key, value in o.items():
+      if isinstance(value, list):
+        print('Is list')
+      elif isinstance(value, dict):
+        print('Is dict')
+      print(type(value))
