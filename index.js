40267ea983c44d85ad3fe6cb09962807f7b49dbf
Zhengqian Kuang <dev.kuang@gmail.com>
2022-12-20 16:48:53 -0600

    fix

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import {App} from './src/components/root/app';
+import {App} from './src/components/root/App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
