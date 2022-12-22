f311f88bf3435304e8dc76c34446e9460bed8538
Zhengqian Kuang <dev.kuang@gmail.com>
2022-12-22 11:01:52 -0600

    test

---
+++ src/scripts/navagation-spec/parse.py
@@ -0,0 +1,19 @@
+# pip3 install pandas
+# pip3 install openpyxl
+# In VSCode, Command-Shift-P, "Python: Select interpreter", select the one marked "Global"
+
+# To run, in Terminal,
+# $ python3 parse.py
+
+import pandas
+
+workbook = pandas.read_excel('navigation-spec-test.xlsx')
+print('workbook:', workbook)
+print('workbook.columns:', workbook.columns)
+print('workbook.columns.values[0]:', workbook.columns.values[0])
+print('workbook["Type"].iloc[0]:', workbook['Type'].iloc[0])
+print('number of rows by `len(workbook)`:', len(workbook)) # number of rows
+print('number of rows by `workbook[workbook.columns[0]].count()`:', workbook[workbook.columns[0]].count())
+print('(number-of-rows, number-of-columns) by `workbook.shape`:', workbook.shape) # (number-of-rows, number-of-columns)
+print('number of rows by `workbook.shape[0]`:', workbook.shape[0]) # number of rows
+print('number of columns by `workbook.shape[1]`:', workbook.shape[1]) # number of columns
